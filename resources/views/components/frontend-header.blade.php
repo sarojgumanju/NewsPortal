f471d28243b5dfec560fde948f9c4021091c5ca3
sarojgumanju <sarojgumanju107@gmail.com>
2026-05-13 22:28:08 +0545

    news portal navbar designing

--- resources/views/components/frontend-header.blade.php
+++ resources/views/components/frontend-header.blade.php
@@ -1,8 +1,43 @@
-<header>
-    <nav>
-        <ul>
-            <li><a href="{{route('home')}}">home</a></li>
-            <li><a href="{{route('about')}}">Abou</a></li>
-        </ul>
+<header class="bg-white">
+
+    <div class="flex justify-around items-center container mx-auto py-2">
+        <div class="">
+            <span class="text-red-500 font-bold text-4xl ">कटुञ्जे </span>
+            <span class=" text-blue-500 text-xl">दैनिक</span>
+            <img class="w-[150px] h-[20px]" src="{{asset('frontend/images/purpleline.png')}}" alt="purple_line">
+        </div>
+        <div>
+            <p class="text-xl">बुधबार, ३० वैशाख २०८३</p>
+        </div>
+    </div>
+
+
+    <div class="bg-gray-100">
+        <nav class="mt-5 container mx-auto flex justify-between items-center p-2">
+
+        <div class="flex gap-20 items-center font-bold">
+           <a href="">fddfdgf</a>
+           <a href="">fdczzgf</a>
+           <a href="">fdggjggf</a>
+           <a href="">fdfdgf</a>
+           <a href="">fdcxrytgf</a>
+        </div>
+
+        <div>
+
+            <form class="max-w-md mx-auto">   
+                <label for="search" class="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" class="font-bold"/></svg>
+                    </div>
+                    <input type="search" id="search" class="block w-full p-3 ps-9 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand shadow-xs placeholder:text-body" placeholder="Search" required />
+                    <button type="button" class="absolute end-1.5 bottom-1.5 text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded text-xs px-3 py-1.5 focus:outline-none">Search</button>        
+
+                </div>
+            </form>
+
+        </div>
     </nav>
+    </div>
 </header>
